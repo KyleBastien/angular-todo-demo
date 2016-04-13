da8a5902ac6a499b0004d1ee4ce3a556e51b97a2
Kyle McNutt <mcnuttkyle93@gmail.com>
2016-04-12 22:28:26 -0700

    Step 3: Adding our first example of two-way databinding on the sortBy property. I also added "age" to each todoList item, this is so we can sort by how old a particular item is.

--- src/client/app/todo/todo.controller.js
+++ src/client/app/todo/todo.controller.js
@@ -8,6 +8,7 @@
   function TodoController() {
     var vm = this;
     vm.title = 'Admin';
+    vm.sortBy = 'age';
 
     activate();
 
@@ -16,15 +17,18 @@
       vm.todoList = [
         {
             title: 'Clean My Room!',
-            description: 'Especially the 20 Chipotle bags in the corner.'
+            description: 'Especially the 20 Chipotle bags in the corner.',
+            age: 3
         },
         {
             title: 'Write Moar Code!',
-            description: 'Cause you just can\'t get enough!'
+            description: 'Cause you just can\'t get enough!',
+            age: 2
         },
         {
             title: 'Sleep',
-            description: 'Hahahahahahahaha!'
+            description: 'Hahahahahahahaha!',
+            age: 1
         }
       ];
     }
