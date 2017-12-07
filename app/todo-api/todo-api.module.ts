9a757df416a91faef8b56024e7d2699e2b4c2a22
Kyle McNutt <kymcnutt@microsoft.com>
2017-12-07 13:34:46 -0800

    chore: Removing unused imports.

--- app/todo-api/todo-api.module.ts
+++ app/todo-api/todo-api.module.ts
@@ -1,7 +1,5 @@
-import * as angular from 'angular'
 import { TodoApi } from './todo-api.service';
 import { NgModule } from '@angular/core';
-import { downgradeInjectable } from '@angular/upgrade/static';
 
 @NgModule({
   providers: [
