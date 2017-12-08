4fcd8f0dd0451357ccc3d4d10eef1a2a7df70108
Kyle McNutt <kymcnutt@microsoft.com>
2017-12-07 17:04:31 -0800

    Fixing e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('angular-todo-demo App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display completed in footer', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toContain('Completed');
   });
 });
